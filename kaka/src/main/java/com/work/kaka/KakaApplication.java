661d41eb6a9a7046874a8a27dfb42392a7fa5ef9
Gaurav-2417 <gaurav.kumar.241708@gmail.com>
2024-04-26 01:48:51 +0530

    With Maven Spring

---
+++ kaka/src/main/java/com/work/kaka/KakaApplication.java
@@ -0,0 +1,13 @@
+package com.work.kaka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakaApplication.class, args);
+	}
+
+}
---
+++ kaka/src/test/java/com/work/kaka/KakaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.work.kaka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
