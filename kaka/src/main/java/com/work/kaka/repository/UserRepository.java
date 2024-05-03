9bc812ae943b4f885c8ada7fcb66f83708eb782c
Gaurav-2417 <gaurav.kumar.241708@gmail.com>
2024-05-04 00:39:57 +0530

    changed user controller mapping links, removed CrudRepository as it is not required

--- kaka/src/main/java/com/work/kaka/repository/UserRepository.java
+++ kaka/src/main/java/com/work/kaka/repository/UserRepository.java
@@ -2,12 +2,11 @@ package com.work.kaka.repository;
 
 import com.work.kaka.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository// Optional, but good for clarity
-public interface UserRepository extends JpaRepository<User, Long>, CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
 
